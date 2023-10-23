7283a8b6e652e215390c77cc67cf98f896145c13
alexbalak21 <alex.balak@outlook.com>
2023-10-23 13:02:20 +0200

    reformated code on navifation.php

--- components/navigation.php
+++ components/navigation.php
@@ -1,49 +1,43 @@
-            <!------------------------------------------------------------ NAVIGATION START------------------------------------------------------------>
-            <nav
-                id="top"
-                class="navbar navbar-expand-lg navbar-dark py-lg-0 px-lg-5 wow fadeIn"
-                data-wow-delay="0.1s"
+<!------------------------------------------------------------ NAVIGATION START------------------------------------------------------------>
+<nav
+    id="top"
+    class="navbar navbar-expand-lg navbar-dark py-lg-0 px-lg-5 wow fadeIn"
+    data-wow-delay="0.1s"
+>
+    <a href="/#header-carousel" class="navbar-brand ms-4 ms-lg-0">
+        <img class="brand-logo" src="img/logo.png" alt="Fly It Logo" />
+    </a>
+    <button
+        type="button"
+        class="navbar-toggler me-4"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarCollapse"
+    >
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+        <div class="navbar-nav ms-auto p-4 p-lg-0">
+            <a href="/#header-carousel" class="nav-item nav-link active"
+                >Accueil</a
             >
-                <a href="/#header-carousel" class="navbar-brand ms-4 ms-lg-0">
-                    <img
-                        class="brand-logo"
-                        src="img/logo.png"
-                        alt="Fly It Logo"
-                    />
-                </a>
-                <button
-                    type="button"
-                    class="navbar-toggler me-4"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#navbarCollapse"
-                >
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarCollapse">
-                    <div class="navbar-nav ms-auto p-4 p-lg-0">
-                        <a href="/#header-carousel" class="nav-item nav-link active" 
-                            >Accueil</a
-                        >
-                        <a href="/#about" class="nav-item nav-link"  data-bs-toggle="collapse navbar-collapse">A propos</a>
+            <a
+                href="/#about"
+                class="nav-item nav-link"
+                data-bs-toggle="collapse navbar-collapse"
+                >A propos</a
+            >
 
-                        <a href="/#tech" class="nav-item nav-link" >Techs</a>
+            <a href="/#tech" class="nav-item nav-link">Techs</a>
 
-                        <a href="/#services" class="nav-item nav-link"
-                            >Services</a
-                        >
+            <a href="/#services" class="nav-item nav-link">Services</a>
 
-                        <a href="/#team" class="nav-item nav-link">Équipe</a>
+            <a href="/#team" class="nav-item nav-link">Équipe</a>
 
-                   
-                        <a href="#partners" class="nav-item nav-link"
-                            >Partenaires
-                        </a>
+            <a href="#partners" class="nav-item nav-link">Partenaires </a>
 
-                        <a href="contact.php" class="nav-item nav-link"
-                            >Contact</a
-                        >
+            <a href="contact.php" class="nav-item nav-link">Contact</a>
 
-                        <!-- <div class="nav-item dropdown">
+            <!-- <div class="nav-item dropdown">
                             <a
                                 href="#"
                                 class="nav-link dropdown-toggle"
@@ -65,20 +59,17 @@
                                 >
                             </div>
                         </div> -->
-                    </div>
-                    <div class="d-none d-lg-flex ms-2">
-                        <a
-                            class="btn btn-outline-primary py-2 px-3"
-                            href="contact.html"
-                        >
-                            Montez à bord
-                            <div
-                                class="d-inline-flex btn-sm-square bg-primary text-secondary rounded-circle ms-2"
-                            >
-                                <i class="fa fa-arrow-right"></i>
-                            </div>
-                        </a>
-                    </div>
+        </div>
+        <div class="d-none d-lg-flex ms-2">
+            <a class="btn btn-outline-primary py-2 px-3" href="contact.html">
+                Montez à bord
+                <div
+                    class="d-inline-flex btn-sm-square bg-primary text-secondary rounded-circle ms-2"
+                >
+                    <i class="fa fa-arrow-right"></i>
                 </div>
-            </nav>
-            <!------------------------------------------------------------ NAVIGATION END------------------------------------------------------------>
+            </a>
+        </div>
+    </div>
+</nav>
+<!------------------------------------------------------------ NAVIGATION END------------------------------------------------------------>
