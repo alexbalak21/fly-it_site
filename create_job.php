a24e37c7aed42bd903186fb650fa879cbf6ea178
alexbalak21 <alex.balak@outlook.com>
2023-11-08 00:02:41 +0100

    create_job control added.

--- create_job.php
+++ create_job.php
@@ -30,39 +30,39 @@ require_once './components/head.php';
         <div class="col-xl-6 col-lg-8 col-md-10 col-sm-12 my-5 create-job">
             <div class="form-floating mb-3">
                 <input id="title" type="text" class="form-control" placeholder="Titre du poste" />
-                <div class="valid-feedback">Ok</div>
-                <div class="invalid-feedback">Entrez le titre du poste</div>
+                <div class="valid-feedback ms-2">Saved</div>
+                <div class="invalid-feedback ms-2">Entrez le titre du poste</div>
                 <label for="title">Titre du poste</label>
             </div>
             <div class="form-floating mb-3">
                 <input id="location" type="text" class="form-control" placeholder="Ville" />
-                <div class="valid-feedback">Ok</div>
-                <div class="invalid-feedback">Entrez une ville</div>
+                <div class="valid-feedback ms-2">Saved</div>
+                <div class="invalid-feedback ms-2">Entrez une ville</div>
                 <label for="location">Ville</label>
             </div>
             <div class="form-floating mb-3">
                 <input id="type" type="text" class="form-control" placeholder="Type de contrat (CDI/CDD)" />
-                <div class="valid-feedback">Ok</div>
-                <div class="invalid-feedback">Entrez le type de poste</div>
+                <div class="valid-feedback ms-2">Saved</div>
+                <div class="invalid-feedback ms-2">Entrez le type de poste</div>
                 <label for="type">Type de contrat (CDI/CDD)</label>
             </div>
             <div class="form-floating mb-3">
                 <input id="time" type="text" class="form-control" placeholder="Temps de Travail (Temps plein / partiel)" />
-                <div class="valid-feedback">Ok</div>
-                <div class="invalid-feedback">Entrez le temps de travail</div>
+                <div class="valid-feedback ms-2">Saved</div>
+                <div class="invalid-feedback ms-2">Entrez le temps de travail</div>
                 <label for="name">Temps de Travail (Temps plein / partiel)</label>
             </div>
             <div class="form-floating mb-3">
                 <input id="salary" type="text" class="form-control" placeholder="Salaire annuel (33k€/an)" />
-                <div class="valid-feedback">Ok</div>
-                <div class="invalid-feedback">Entrez le salaire</div>
+                <div class="valid-feedback ms-2">Saved</div>
+                <div class="invalid-feedback ms-2">Entrez le salaire</div>
                 <label for="salary">Salaire annuel (33k€/an)</label>
             </div>
             <div class="d-flex">
                 <div class="form-floating mb-1 col-9">
                     <input id="tech" type="text" class="form-control" />
-                    <div class="valid-feedback">Ok</div>
-                    <div class="invalid-feedback">Entrez une Techno</div>
+                    <div class="valid-feedback ms-2">Saved</div>
+                    <div class="invalid-feedback ms-2">Existe déjà.</div>
                     <label for="tech">Technologie</label>
                 </div>
                 <button class="btn btn-secondary mx-4 my-2" id="add_tech" style="height: 40px">
@@ -73,14 +73,23 @@ require_once './components/head.php';
             <div class="badge-container mb-4"></div>
             <div class="form-floating mb-3">
                 <textarea rows="20" id="job_description" class="form-control" placeholder="Description"></textarea>
-                <div class="valid-feedback">Ok</div>
-                <div class="invalid-feedback">Entrez une description</div>
+                <div class="valid-feedback ms-2">Saved</div>
+                <div class="invalid-feedback ms-2">Entrez une description</div>
                 <label for="name">Description</label>
             </div>
-            <div class="text-center"> <button class="btn btn-secondary my-2" id="add_job" style="height: 40px">
+            <div id="alert-containber"></div>
+            <div class="btn-container text-center">
+                <button class="btn btn-secondary my-2" id="add_job" style="height: 40px">
                     Ajouter Poste &nbsp;
-                    <!-- <i class="fa-solid fa-circle-check" style="font-size: 18px;"></i> -->
-                    <i class="fa-solid fa-plus" style="font-size: 18px;"></i>
+                    <i class="fa-solid fa-file-circle-plus"></i>
+                </button>
+                <button class="btn btn-success my-2" id="add_job" style="height: 40px">
+                    Modifier &nbsp;
+                    <i class="fa-solid fa-pen"></i>
+                </button>
+                <button class="btn btn-warning my-2" id="reset_from" style="height: 40px">
+                    Remetre à zero &nbsp;
+                    <i class="fa-regular fa-file"></i>
                 </button>
 
             </div>
--- js/jobs.js
+++ js/jobs.js
@@ -3,13 +3,31 @@ const job_location = document.getElementById("location")
 const job_type = document.getElementById("type")
 const time = document.getElementById("time")
 const job_salary = document.getElementById("salary")
+const tech = document.getElementById("tech")
+const add_tech = document.getElementById("add_tech")
+const badge_container = document.querySelector(".create-job .badge-container")
 const job_description = document.getElementById("job_description")
-const add_job = document.getElementById("add_job")
+const add_job_btn = document.getElementById("add_job")
+const inputs = document.querySelectorAll(".create-job input")
+const alert_container = document.getElementById("alert-containber")
+const reset_form_btn = document.getElementById("reset_from")
+
+const techs = new Set()
+let form_locked = false
+
+function generate_alert(alert_type = "success", message = "Le poste a bien été crée.") {
+    const alert = `           
+<div class="alert alert-${alert_type} alert-dismissible fade show text-center" role="alert">
+<p>${message}</p>
+<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>`
+    return alert
+}
 
 const job_url = "/logic/job.php"
 
-add_job.addEventListener("click", () => {
-    const res = create_job(
+add_job_btn.addEventListener("click", async () => {
+    const res = await send_create_job_request(
         title.value,
         job_location.value,
         job_type.value,
@@ -18,11 +36,52 @@ add_job.addEventListener("click", () => {
         techs,
         job_description.value
     )
-    console.log(res)
-    if (res == 201) console.log("Created")
+    if (res == 201) {
+        validate_form()
+        alert_container.innerHTML = generate_alert("success", "Le poste a bien été crée avec succes.")
+    } else {
+        alert_container.innerHTML = generate_alert(
+            "danger",
+            "Il y a eu un probleme avec la base de donnés.<br>Contactez l'administrateur du site."
+        )
+    }
 })
 
-async function create_job(title, location, type, time, salary, techs, description) {
+reset_form_btn.addEventListener("click", () => {
+    reset_job_form()
+})
+
+function validate_form() {
+    form_locked = true
+    inputs.forEach((input) => {
+        input.classList.add("is-valid")
+        input.setAttribute("disabled", "")
+    })
+    job_description.classList.add("is-valid")
+    job_description.setAttribute("disabled", "")
+    add_job_btn.setAttribute("disabled", "")
+    add_tech.setAttribute("disabled", "")
+}
+
+function reset_job_form() {
+    inputs.forEach((input) => {
+        input.value = ""
+        input.classList.remove("is-valid")
+        input.removeAttribute("disabled")
+    })
+    job_description.classList.remove("is-valid")
+    job_description.removeAttribute("disabled")
+    job_description.value = ""
+    job_description.innerText = ""
+    add_job_btn.removeAttribute("disabled")
+    add_tech.removeAttribute("disabled")
+    document.querySelectorAll(".tech-badge").forEach((tech_badge) => tech_badge.remove())
+    techs.clear()
+    alert_container.innerHTML = ""
+    form_locked = false
+}
+
+async function send_create_job_request(title, location, type, time, salary, techs, description) {
     const techsArray = [...techs]
     let formData = new FormData()
     formData.append("title", title)
@@ -37,20 +96,15 @@ async function create_job(title, location, type, time, salary, techs, descriptio
         body: formData,
     })
     //RETURN THE STATUS CODE 400: DATA MISSING / 404: USER NOT FOUND / 403: BAD PASSWORD / 202: ACCEPTED
-    return response.statusCode
+    return response.status
 }
-S
 
-function cahge_button_color() {
-    add_job.classList.remove("btn-secondary")
-    add_job.classList.add("btn-success")
+function chage_btn_to_success() {
+    add_job_btn.classList.remove("btn-secondary")
+    add_job_btn.classList.add("btn-success")
 }
 
 //ADD TECH AND BADGE
-const tech = document.getElementById("tech")
-const add_tech = document.getElementById("add_tech")
-const badge_container = document.querySelector(".create-job .badge-container")
-const techs = new Set()
 
 function close_badge_btn() {
     const close_badge_btn = document.createElement("i")
@@ -65,7 +119,7 @@ function generate_badge(name = "") {
     text.innerText = name
     badge.appendChild(text)
     badge.innerText = name
-    badge.className = "badge bg-grey"
+    badge.className = "badge bg-grey tech-badge"
     const close = close_badge_btn()
     badge.appendChild(close)
     close.addEventListener("click", () => remove_tech_badge(badge, name))
@@ -73,14 +127,20 @@ function generate_badge(name = "") {
 }
 
 function add_tech_badge(name = "JavaScript") {
+    name = name.toUpperCase()
+    if (techs.has(name)) {
+        tech.classList.add("is-invalid")
+        return null
+    }
     const badge = generate_badge(name)
     badge_container.appendChild(badge)
     techs.add(name)
     tech.value = ""
-    console.log(techs)
+    tech.classList.remove("is-invalid")
 }
 
 function remove_tech_badge(badge, name = "") {
+    if (form_locked) return null
     badge.remove()
     techs.delete(name)
     tech.value = name
@@ -91,6 +151,8 @@ tech.addEventListener("keyup", (e) => {
     if (e.key === "Enter" && tech.value) add_tech_badge(tech.value)
 })
 
-add_tech.addEventListener("click", () => {
-    tech.value && add_tech_badge(tech.value)
+add_tech.addEventListener("keyup", (e) => {
+    if (e.key === "Enter" && tech.value) add_tech_badge(tech.value)
 })
+
+add_tech.addEventListener("click", () => {})
